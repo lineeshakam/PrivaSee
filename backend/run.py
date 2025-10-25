fb2de91644bcfbcc8cd5af8001ab8aba5984a995
lineeshakam <lpk5305@psu.edu>
2025-10-25 16:53:05 -0400

    run.py file FOR BACKEND

---
+++ backend/run.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_cors import CORS
+from .routes import bp as api_bp
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app)  # lets the extension call localhost during dev
+    app.register_blueprint(api_bp)
+    return app
